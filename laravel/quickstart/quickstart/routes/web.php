2623fd15b5407b26e5e6bb05cc19423ac00f6e58
GiMuhai <gildajanye@gmail.com>
2019-02-28 14:59:03 +0200

    Second commit

--- laravel/quickstart/quickstart/routes/web.php
+++ laravel/quickstart/quickstart/routes/web.php
@@ -1,42 +1,24 @@
 <?php
-use App\Task;
-use Illuminate\Http\Request;
-/**
- * Display All Tasks
- */
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/', function () {
-   $tasks = Task::orderBy('created_at', 'asc')->get();
-   
-   return view('tasks', [
-        'tasks' => $tasks
-    ]);
+    return view('welcome');
 });
 
-/**
- * Add A New Task
- */
-Route::post('/task', function (Request $request) {
-     $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
+Auth::routes();
 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
+Route::get('/home', 'HomeController@index')->name('home');
 
-  $task = new Task;
-    $task->name = $request->name;
-    $task->save();
+Auth::routes();
 
-    return redirect('/');
-});
-/**
- * Delete An Existing Task
- */
-Route::delete('/task/{id}', function ($id) {
-    Task::findOrFail($id)->delete();
-
-    return redirect('/');
-});
+Route::get('/home', 'HomeController@index')->name('home');
